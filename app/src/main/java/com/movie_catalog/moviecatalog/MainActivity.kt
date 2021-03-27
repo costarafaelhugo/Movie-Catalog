b7212f94fbafed2e858f7fd07222a878561e029e
Hugo Costa <costarafaelhugo@gmail.com>
2021-03-27 18:23:35 -0300

    vai tudo

---
+++ app/src/main/java/com/movie_catalog/moviecatalog/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.movie_catalog.moviecatalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cronocode.moviecatalog.R
+import com.movie_catalog.moviecatalog.models.Movie
+import com.movie_catalog.moviecatalog.models.MovieResponse
+import com.movie_catalog.moviecatalog.services.MovieApiInterface
+import com.movie_catalog.moviecatalog.services.MovieApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rv_movies_list.layoutManager = LinearLayoutManager(this)
+        rv_movies_list.setHasFixedSize(true)
+        getMovieData { movies : List<Movie> ->
+            rv_movies_list.adapter = MovieAdapter(movies)
+        }
+    }
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                return callback(response.body()!!.movies)
+            }
+
+        })
+    }
+}
